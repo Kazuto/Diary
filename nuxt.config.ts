caa9c8fb6d55ac40d8ad2a8387d39de92bb9f8bf
Kazuto <mail@kazuto.de>
2024-06-09 19:50:18 +0200

    feat(content): add nuxt content module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint", "@nuxtjs/tailwindcss"],
+  modules: ["@nuxt/eslint", "@nuxtjs/tailwindcss", "@nuxt/content"],
 
   tailwindcss: {
     cssPath: "~/assets/app.css",
