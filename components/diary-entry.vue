1a9c869d52198a521d5e655462f9f964e3002237
Kazuto <mail@kazuto.de>
2024-06-19 20:05:06 +0200

    lint

--- components/diary-entry.vue
+++ components/diary-entry.vue
@@ -1,9 +1,10 @@
 <template>
   <NuxtLink
     :to="doc._path"
-    class="duration-250 block rounded-lg border border-black/30  px-4 py-3 font-semibold shadow-md shadow-gray-900/10 ring-inset transition-all hover:-translate-y-0.5 hover:shadow-md dark:ring-1"
+    class="duration-250 block rounded-lg border border-black/30 px-4 py-3 font-semibold shadow-md shadow-gray-900/10 ring-inset transition-all hover:-translate-y-0.5 hover:shadow-md dark:ring-1"
     :class="{
-      'bg-secondary-100/[0.025] ring-white/5 hover:bg-secondary-100/[0.05]': category != 'thoughts',
+      'bg-secondary-100/[0.025] ring-white/5 hover:bg-secondary-100/[0.05]':
+        category != 'thoughts',
       'bg-red-500/[0.02] ring-red-700/10 hover:bg-red-500/[0.05]':
         category == 'thoughts',
     }"
