22691575d0552b68896b04bd5c9bc197bfe5c87f
Kai Mayer <mail@kazuto.de>
2024-06-25 08:09:26 +0200

    Update diary-entry.vue

--- components/diary-entry.vue
+++ components/diary-entry.vue
@@ -11,7 +11,7 @@
   >
 
 
-<h2 class="text-xl shrink 0 mb-1" v-if="category != 'thoughts' ">
+<h2 class="text-xl shrink 0 mb-2" v-if="category != 'thoughts' ">
 {{ doc.title}} 
 </h2>
 
@@ -19,8 +19,8 @@
       {{ doc.description }}
     </p>
 
-<div class="flex w-full items-start justify-between mt-1">
-<spam class="grow-1" />
+<div class="flex w-full justify-between">
+<span class="grow-1" />
     <span class="justify-self-end block shrink-0 text-xs text-white/40 text-right">
       {{ $formatter.date(doc.created_at) }}
     </span>
