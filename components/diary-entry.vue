54d58174feae63b711f688cae9c23e44d35f5eb2
Kazuto <mail@kazuto.de>
2024-06-25 22:30:00 +0200

    update diary-entry.vue

--- components/diary-entry.vue
+++ components/diary-entry.vue
@@ -5,7 +5,7 @@
     :class="{
       'bg-secondary-100/[0.025] hover:bg-secondary-100/[0.05]':
         category != 'thoughts',
-      'bg-neutral-600/[0.025] hover:bg-neutral-600/[0.05]':
+      'bg-neutral-800/[0.025] hover:bg-neutral-800/[0.05]':
         category == 'thoughts',
     }"
   >
